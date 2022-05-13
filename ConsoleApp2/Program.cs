1336159c9dc7fc2f41e3b7400122452f77536179
Francisco Salvador Diaz Ortiz <N00244919@upn.pe>
2022-05-12 22:20:07 -0500

    eh implementado la primera parte del programa

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1 variables
+            int num1;
+            int num2;
+            int resSuma;
+            int resResta;
+            int resMulti;
+            int resDiv;
+            //2.pedir datos al usuario
+            Console.WriteLine("ingresa el primer numero: ");
+            num1=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("ingresa el segundo numero: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+
+
+        }
+    }
+}
